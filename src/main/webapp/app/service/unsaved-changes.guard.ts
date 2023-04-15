54e0887185b88de3281bb74789de36f9d125db63
Stefan Jucker <stefan.jucker@gmail.com>
2023-04-15 11:00:51 +0200

    Replace deprecated guards

--- src/main/webapp/app/service/unsaved-changes.guard.ts
+++ src/main/webapp/app/service/unsaved-changes.guard.ts
@@ -1,17 +1,14 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {VideoReportComponent} from "../video-report/video-report.component";
 
 @Injectable({
     providedIn: 'root'
 })
-export class UnsavedChangesGuard implements CanDeactivate<VideoReportComponent> {
+export class UnsavedChangesGuard {
 
-    canDeactivate(component: VideoReportComponent,
-                  currentRoute: ActivatedRouteSnapshot,
-                  currentState: RouterStateSnapshot,
-                  nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    canDeactivate(component: VideoReportComponent): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
         return component.canDeactivate();
     }
 
--- src/main/webapp/app/service/unsaved-replies.guard.ts
+++ src/main/webapp/app/service/unsaved-replies.guard.ts
@@ -1,18 +1,14 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
-import {VideoReportComponent} from "../video-report/video-report.component";
 import {DiscussVideoReportComponent} from "../discuss-report/discuss-video-report.component";
 
 @Injectable({
     providedIn: 'root'
 })
-export class UnsavedRepliesGuard implements CanDeactivate<DiscussVideoReportComponent> {
+export class UnsavedRepliesGuard {
 
-    canDeactivate(component: DiscussVideoReportComponent,
-                  currentRoute: ActivatedRouteSnapshot,
-                  currentState: RouterStateSnapshot,
-                  nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    canDeactivate(component: DiscussVideoReportComponent): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
         return component.canDeactivate();
     }
 
