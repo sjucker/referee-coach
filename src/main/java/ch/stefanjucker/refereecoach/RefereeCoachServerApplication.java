7421ff4c7a897b25af401c8639c51282120f803f
Stefan Jucker <stefan.jucker@gmail.com>
2023-10-21 13:40:43 +0200

    Enable scheduling

--- src/main/java/ch/stefanjucker/refereecoach/RefereeCoachServerApplication.java
+++ src/main/java/ch/stefanjucker/refereecoach/RefereeCoachServerApplication.java
@@ -3,9 +3,11 @@ package ch.stefanjucker.refereecoach;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class RefereeCoachServerApplication {
 
     public static void main(String[] args) {
